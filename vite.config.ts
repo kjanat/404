c5237cae04ee8c589b053ba8fa5b8ef9648d8a35
Kaj Kowalski <info@kajkowalski.nl>
2026-02-26 17:17:07 +0100

    fix: disable Cloudflare Rocket Loader on module script
    Rocket Loader rewrites script type='module' to a random token,
    preventing the JS from executing. data-cfasync='false' opts out.

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,12 @@ export default defineConfig({
 			input: 'src/icon.svg',
 			emit: { source: { name: 'favicon.svg' } },
 		}),
+		{
+			name: 'cf-async-disable',
+			transformIndexHtml(html) {
+				return html.replace(/<script type="module"/g, '<script data-cfasync="false" type="module"');
+			},
+		},
 	],
 	build: { sourcemap: true },
 	server: {
